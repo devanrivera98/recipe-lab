6ace00919dfa3a1ecf00b45220fc18dfe6ea53c4
Devan Rivera <devanrivera98@gmail.com>
2025-01-04 23:49:14 +0000

    moved contact strip that was intented to be a footer to homepage component

--- recipe-lab/src/app/page.tsx
+++ recipe-lab/src/app/page.tsx
@@ -1,13 +1,12 @@
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
-import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HomePage />
-      <Footer />
+
     </div>
   );
 }
--- recipe-lab/src/app/pages/HomePage.tsx
+++ recipe-lab/src/app/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import TrendingMainPage from '../sections/Home/components/TrendingMainRecipe'
 import OrangeCranberryHero from '../sections/Home/components/OrangeCranberryHero'
 import WeeknightWinnersRecipes from '../sections/Home/components/WeeknightWinnersRecipes'
 import BreakfastIdeas from '../sections/Home/components/BreakfastIdeas'
+import ContactStrip from '../sections/Home/components/ContactStrip'
 // import useSearchApi from '../../hooks/useSearchApi'
 
 export default function HomePage() {
@@ -22,6 +23,7 @@ export default function HomePage() {
       <div className="container mx-auto">
         <WeeknightWinnersRecipes />
       </div>
+      <ContactStrip />
     </>
   )
 }
