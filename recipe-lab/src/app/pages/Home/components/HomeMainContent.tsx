6e0d1d902c42935a67b7e14d50d99b735bd9be53
Devan Rivera <devanrivera98@gmail.com>
2025-01-16 00:02:05 +0000

    added text to complement main page card

--- recipe-lab/src/app/pages/Home/components/HomeMainContent.tsx
+++ recipe-lab/src/app/pages/Home/components/HomeMainContent.tsx
@@ -3,7 +3,13 @@ export default function HomeMainContent() {
   return (
     <>
       <section className="home-top-of-page pt-16 px-2 md:px-0  mb-16">
+        <div className="hosting-card cursor-pointer">
           <img alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
+          <div className="py-1">
+            <h1 className="hosting-card-title text-3xl py-2">Discover the Best Recipes: Perfect for Hosting Every Occasion</h1>
+            <div>Sometimes there just aren't enough hours in the day. Here are some great quick and tasty meals that anyone can make in less than 15 minutes.</div>
+          </div>
+        </div>
           <div className="bg-fadedGreen p-6">
             <h1 className='text-2xl font-bold text-center pb-6'>Food News</h1>
             <div className='grid sm:grid-cols-1 lg:grid-cols-1 md:grid-cols-2 gap-10'>
