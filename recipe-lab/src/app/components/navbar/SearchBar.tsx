20ca53998bb28d57d64040a18e082eb730c67b16
Devan Rivera <devanrivera98@gmail.com>
2025-01-31 23:00:09 +0000

    applied api functionality to search bar

--- recipe-lab/src/app/components/navbar/SearchBar.tsx
+++ recipe-lab/src/app/components/navbar/SearchBar.tsx
@@ -4,14 +4,18 @@ import { useState } from "react";
 
 export default function SearchBar() {
 
-  const [query, setquery] = useState();
-  const [recipes, setRecipes] = useState();
+  const [query, setQuery] = useState('');
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
-    const data = await getSpoonRecipes('rice');
+    const data = await getSpoonRecipes(query);
     console.log(data)
   }
+
+  function handleRecipeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
+  }
+
 
   return (
     <>
@@ -20,6 +24,7 @@ export default function SearchBar() {
     <input
       className="flex-grow pl-1 rounded"
       placeholder="Find a recipe"
+      onChange={handleRecipeInput}
     ></input>
     <button className="absolute bg-lightOrange hover:bg-darkOrange px-3 h-full right-0 rounded">
       <FaMagnifyingGlass color="white" />
